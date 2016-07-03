b6b3c2149be83c655f87c195e2c22dcad3f33c16
Athens Zhang <athens314@hotmail.com>
2016-07-03 01:52:02 -0400

    set session attr at UserController

--- src/com/adogo/uaas/controller/UserController.java
+++ src/com/adogo/uaas/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.SessionAttributes;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.adogo.uaas.entity.User;
@@ -25,6 +26,7 @@ import com.athensoft.common.email.service.EmailService;
  *
  */
 @Controller
+@SessionAttributes("userAccount")
 public class UserController {
 	
 	private UserService userService;
