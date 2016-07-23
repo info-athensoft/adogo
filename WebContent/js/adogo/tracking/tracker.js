9e84d511779cf9f9cd5392b09cd035f08e82d113
KK_2 <KK_2@Deng_Zhu>
2016-07-23 17:13:47 -0400

    add click tracking

--- WebContent/js/adogo/tracking/tracker.js
+++ WebContent/js/adogo/tracking/tracker.js
@@ -1,14 +1,29 @@
 /*tracker v1.00*/
 
+//GA tracking code
+(function(i, s, o, g, r, a, m) {
+	i['GoogleAnalyticsObject'] = r;
+	i[r] = i[r] || function() {
+		(i[r].q = i[r].q || []).push(arguments)
+	}, i[r].l = 1 * new Date();
+	a = s.createElement(o), m = s.getElementsByTagName(o)[0];
+	a.async = 1;
+	a.src = g;
+	m.parentNode.insertBefore(a, m)
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js',
+		'ga');
 
-  //GA tracking code
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-  
-  //page views
-  ga('create', 'UA-77264108-1', 'auto');
-  ga('set', 'title', data_layer.page_name);
-  ga('send', 'pageview');
-  
+// page views
+ga('create', 'UA-77264108-1', 'auto');
+ga('set', 'title', data_layer.page_name);
+ga('send', 'pageview');
+
+// click
+function track(data) {
+	if (!!data.id === 1) {
+		ga('send', 'event', 'Adveriser profile page:', data.mid);		
+	}
+	if (!!data.id === 2) {
+		ga('send', 'event', 'Adveriser homepage:', data.mid);		
+	}
+}
