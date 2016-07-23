7bbc62efa63427fadc079580fc843870e1b203dd
KK_2 <KK_2@Deng_Zhu>
2016-07-23 16:39:47 -0400

    Add tracking code on category pages

--- WebContent/js/adogo/tracking/tracker.js
+++ WebContent/js/adogo/tracking/tracker.js
@@ -6,6 +6,9 @@
   (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
   m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
   })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
-
+  
+  //page views
   ga('create', 'UA-77264108-1', 'auto');
+  ga('set', 'title', data_layer.page_name);
   ga('send', 'pageview');
+  
