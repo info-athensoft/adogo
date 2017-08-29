9d18a35cd313e783936ff1183d618052e82c1466
athens <athens314@hotmail.com>
2017-08-28 20:31:53 -0400

    change port from 8088 to 8080

--- src/test/com/adogo/restapi/client/Main.java
+++ src/test/com/adogo/restapi/client/Main.java
@@ -55,7 +55,8 @@ public class Main
 
             //String uri = new String("http://" + mRESTServer.getHost() + ":8080/springmvc-resttemplate-test/api/{id}");
             //String uri = new String("http://127.0.0.1:8088/api/{id}");
-            String uri = new String("http://127.0.0.1:8088/acp/useracct-test/{id}");
+            //String uri = new String("http://127.0.0.1:8088/acp/useracct-test/{id}");
+            String uri = new String("http://127.0.0.1:8080/acp/useracct-test/{id}");
             //String uri = new String("http://127.0.0.1:8088/api/{name}");
 
 //            User returns = rt.postForObject(uri, u, User.class, vars);
@@ -66,7 +67,8 @@ public class Main
             System.out.println("Get User:  " + getResult.toString());
             
             //test post api
-            String postUri = new String("http://127.0.0.1:8088/acp/useracct-test");
+//            String postUri = new String("http://127.0.0.1:8088/acp/useracct-test");
+            String postUri = new String("http://127.0.0.1:8080/acp/useracct-test");
             UserAccount userAccount = new UserAccount();
     		userAccount.setAcctName("f1@gmail.com");
     		userAccount.setPassword("123");
