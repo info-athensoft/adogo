dc7d7029f2d99db09ba6f1b8149515b42704d5a3
athens <athens314@hotmail.com>
2017-09-02 20:05:01 -0400

    wrote test for index

--- src/com/adogo/general/controller/TestController.java
+++ src/com/adogo/general/controller/TestController.java
@@ -10,4 +10,35 @@ public class TestController {
 	public String showProfile(){
 		return "profile";
 	}
+	
+	/**
+	 * original design, display all booth by category or other criteria
+	 * @return
+	 */
+	@RequestMapping("/test/index/1")
+	public String testIndexV1(){
+		return "index-v1";
+	}
+	
+	
+	/**
+	 * upgraded booth design including details on it
+	 * design layout for ad post thread on index page
+	 * upgraded page layout for presenting both highlighted ad post threads and highlighted booths
+	 * @return
+	 */
+	@RequestMapping("/test/index/2")
+	public String testIndexV2(){
+		return "index-v2";
+	}
+	
+	/**
+	 * refined category menu
+	 * opened search bar
+	 * @return
+	 */
+	@RequestMapping("/test/index/3")
+	public String testIndexV3(){
+		return "index";
+	}
 }
