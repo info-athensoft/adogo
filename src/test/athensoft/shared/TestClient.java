32e76d3ef2b73a326c993dfdb5219ebf7f316d5b
athens <athens314@hotmail.com>
2017-09-12 18:59:29 -0400

    changed test client

---
+++ src/test/athensoft/shared/TestClient.java
@@ -0,0 +1,12 @@
+package test.athensoft.shared;
+
+public class TestClient {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+	
+//	TestSharedClass t = new TestSharedClass();
+//	t.showTestShared();
+}
